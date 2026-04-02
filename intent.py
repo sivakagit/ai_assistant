f0e37268872ce6e530ba0e6b614613f83a9a11ce
SIva K A <sivaflowx13@gmail.com>
2026-04-02 17:19:27 +0530

    "fixes"

--- intent.py
+++ intent.py
@@ -301,7 +301,54 @@ def detect_intent(text):
 
         return "kill_process"
 
+# ---------- ACCESSIBILITY NAVIGATION ----------
 
+    if text in [
+        "read focus",
+        "current item",
+        "what is selected"
+]:
+        return "read_focus"
+
+    if text in [
+        "next",
+        "next item",
+        "next element"
+]:
+        return "next_element"
+
+    if text in [
+        "previous",
+        "previous item",
+        "go back"
+]:
+        return "previous_element"
+
+    if text in [
+        "click",
+        "activate",
+        "press enter"
+]:
+        return "activate_element"
+
+    if text.startswith("type "):
+        return "type_text"
+
+    if text in [
+        "scroll down"
+]:
+        return "scroll_down"
+
+    if text in [
+        "scroll up"
+]:
+        return "scroll_up"
+
+    if text in [
+        "where am i",
+        "current window"
+]:
+        return "where_am_i"
 
     # ---------- SCREEN READER ----------
 
@@ -345,125 +392,6 @@ def detect_intent(text):
         "show last screen",
     ]):
         return "last_screen"
-
-
-    # ---------- SCREEN EXPLANATION ----------
-
-    if any(phrase in text for phrase in [
-        "explain screen",
-        "explain my screen",
-        "describe screen",
-        "describe my screen",
-        "what is on my screen",
-        "what's on my screen",
-        "whats on my screen",
-        "what do you see on screen",
-        "tell me what's on screen",
-        "tell me what is on screen",
-        "what app is open",
-        "what application is open",
-        "what window is open",
-        "what am i looking at",
-        "describe what you see",
-        "explain what you see",
-    ]):
-        return "explain_screen"
-
-    # ---------- HANDWRITING READER ----------
-
-    if any(phrase in text for phrase in [
-        "read handwriting",
-        "read handwritten",
-        "read this handwritten",
-        "ocr handwriting",
-        "scan handwriting",
-        "read old book",
-        "read scanned book",
-        "read scan",
-        "read image text",
-        "extract handwriting",
-        "correct ocr",
-        "fix ocr",
-    ]):
-        return "read_handwriting"
-
-    # ---------- KEYBOARD NAVIGATION ----------
-
-    if any(phrase in text for phrase in [
-        "what is focused",
-        "what has focus",
-        "what control is focused",
-        "read focused element",
-        "what element is focused",
-        "whats focused",
-        "read focus",
-        "current focus",
-    ]):
-        return "nav_focus"
-
-    if any(phrase in text for phrase in [
-        "what window is this",
-        "what window am i in",
-        "read window title",
-        "current window",
-        "active window",
-        "window title",
-        "what program is this",
-    ]):
-        return "nav_title"
-
-    if any(phrase in text for phrase in [
-        "list windows",
-        "list open windows",
-        "what windows are open",
-        "show all windows",
-        "open windows",
-        "what is open",
-        "what programs are open",
-        "what apps are running",
-    ]):
-        return "nav_windows"
-
-    if any(phrase in text for phrase in [
-        "read clipboard",
-        "what is in clipboard",
-        "clipboard contents",
-        "what did i copy",
-        "paste contents",
-        "show clipboard",
-    ]):
-        return "nav_clipboard"
-
-    if any(phrase in text for phrase in [
-        "toggle auto announce",
-        "auto announce",
-        "enable auto read",
-        "disable auto read",
-        "start reading focus",
-        "stop reading focus",
-        "turn on screen reader",
-        "turn off screen reader",
-        "enable screen reader",
-        "disable screen reader",
-    ]):
-        return "nav_auto"
-
-    if any(phrase in text for phrase in [
-        "next window",
-        "switch window",
-        "alt tab",
-        "go to next window",
-        "switch to next window",
-    ]):
-        return "nav_next_window"
-
-    if any(phrase in text for phrase in [
-        "previous window",
-        "go back window",
-        "previous app",
-        "switch to previous window",
-    ]):
-        return "nav_prev_window"
 
     # ---------- DEFAULT ----------
 
